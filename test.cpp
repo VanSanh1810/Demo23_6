66bb39ddaedbdb16a1084a8537b3720de7fbe7f1
unknown <ngocnam2002.andanh@gmail.com>
2022-06-23 21:46:11 +0700

    tru

--- test.cpp
+++ test.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
+
+void TinhTru(int a, int b);
 int main()
 {
 	int a, b;
 	cin >> a,b;
 	return 0;
 }
+int TinhTru(int a, int b)
+{
+	int c=a-b;
+	cout<< c<< endl;
+	return c;
+	
+}
+
 
