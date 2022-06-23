1c3107ae4531dbb0068139e4bef0595b5c898644
sanh nguyen <sanhnguyen734@gmail.com>
2022-06-23 21:52:25 +0700

    chia

--- test.cpp
+++ test.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
-
+void chia(int a, int b);
 int main()
 {
 	int a, b;
 	cin >> a,b;
+	chia(a,b);
 	return 0;
+}
+
+void chia(int a, int b){
+	cout << a/b;
 }
 
