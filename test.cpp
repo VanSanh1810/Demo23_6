dc0d8ba22ad91db5c6b5d946d535685447fc9012
Son Nguyen <sonnguyenhong382@gmail.com>
2022-06-23 21:49:29 +0700

    final

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
-
-
-int  TinhTru(int a, int b);
+int tinhtong(int a, int b);
+float tinhnhan(int a, int b);
+int TinhTru(int a, int b);
 int main()
 {
 	int a, b;
@@ -12,15 +12,18 @@ int main()
 }
 int TinhTru(int a, int b)
 {
-	int c=a-b;
-	cout<< c<< endl;
+	int c = a - b;
+	cout << c << endl;
 	return c;
-	
 }
-
 
 int tinhtong(int a, int b)
 {
 	cout << a + b;
 	return a + b;
+}
+float tinhnhan(int a, int b)
+{
+	float d = a * b;
+	return d;
 }
