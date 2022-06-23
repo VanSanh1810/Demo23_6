d744470b9419d49bf93a1e746b7a6251e1167123
Son Nguyen <sonnguyenhong382@gmail.com>
2022-06-23 21:45:22 +0700

    haha

--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
-
+int tinh_Tong(int a, int b);
 int main()
 {
 	int a, b;
-	cin >> a,b;
+	cin >> a, b;
 	return 0;
 }
-
+int tinh_Tong(int a, int b)
+{
+	int s = 0;
+}
