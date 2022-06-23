f65b8378365c3e8a26213434ad7d3449ddc84fe5
KhaiHASO <khaikhai331@gmail.com>
2022-06-23 21:43:15 +0700

    tính tổng

--- test.cpp
+++ test.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
 	int a, b;
-	cin >> a,b;
+	cin >> a>>b;
+	tinhtong(a,b);
 	return 0;
+}
+
+int tinhtong(int a, int b)
+{
+	cout<<a+b;
+	return a+b;
 }
 
